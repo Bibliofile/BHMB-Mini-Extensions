35797dbac6cac10b71b34af9d8f77f38966fc1e0
Bibliofile <bibliofilegit@gmail.com>
2019-04-22 20:07:21 -0600

    Create ban_messages.js

---
+++ ban_messages.js
@@ -0,0 +1,22 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('@bhmb/bot')) :
+  typeof define === 'function' && define.amd ? define(['@bhmb/bot'], factory) :
+  (factory(global['@bhmb/bot']));
+}(this, (function (bot) { 'use strict';
+  const MessageBot = bot.MessageBot
+  
+  MessageBot.registerExtension('bibliofile/ban_messages', (ex, world) => {
+    function listener({ player, message }) {
+      if (!message.toLocaleUpperCase().startsWith('/BAN ')) return
+      const banned = message.substr(5)
+      if (!player.isStaff) return
+      if (/\d{2,3}\.\d{2,3}\.\d{2,3}\.\d{2,3}/.test(banned)) return
+      ex.bot.send(`{{Name}} banned {{banned}}`, { name: player.name, banned })
+    }
+
+    world.onMessage.sub(listener)
+
+    ex.remove = () => world.onMessage.unsub(listener)
+  })
+  
+})))
